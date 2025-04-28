87479c14687ef4812bd01a667aa32e224abd86a8
Thales <thalesbarbosa.barbosa10@gmail.com>
2025-04-28 19:54:34 -0300

    teste

---
+++ node_prova/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Rota principal
+app.get('/', (req, res) => {
+    res.send('<h1>Bem-vindo à Página Inicial</h1>');
+});
+
+// Rota /sobre
+app.get('/sobre', (req, res) => {
+    res.send('<h1>Sobre Nós</h1><p>Esta é uma aplicação Node.js.</p>');
+});
+
+// Rota /contato
+app.get('/contato', (req, res) => {
+    res.send('<h1>Contato</h1><p>Email: contato@exemplo.com</p>');
+});
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+    console.log("Servidor rodando em http://localhost:${PORT}");
+});
